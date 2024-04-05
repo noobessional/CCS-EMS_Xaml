114d71781904d8110d17073bdaf2c3f756edf754
noobessional <joemarygot@gmail.com>
2024-04-05 13:07:16 +0800

    ikaw lay bahala sa login mens

--- CCS-EMS/CCS-EMS/MainWindow.xaml.cs
+++ CCS-EMS/CCS-EMS/MainWindow.xaml.cs
@@ -24,5 +24,49 @@ namespace CCS_EMS
         {
             InitializeComponent();
         }
+        private void RemovePlaceholderText(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == "Username")
+            {
+                textBox.Text = "";
+            }
+        }
+
+        private void RestorePlaceholderText(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "Username";
+            }
+        }
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordPlaceholder.Visibility = Visibility.Collapsed;
+            PasswordPlaceholder.IsHitTestVisible = false;
+        }
+
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(PasswordBox.Password))
+            {
+                PasswordPlaceholder.Visibility = Visibility.Visible;
+                PasswordPlaceholder.IsHitTestVisible = false;
+            }
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(PasswordBox.Password))
+            {
+                PasswordPlaceholder.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                PasswordPlaceholder.Visibility = Visibility.Visible;
+            }
+        }
+
     }
 }
